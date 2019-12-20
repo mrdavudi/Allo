64361d93a978ff2c11f9d1487f8018b044be4023
unknown <mohamadrezadavoudi.11@gmail.com>
2019-12-20 11:55:54 +0330

    add ConversationList

--- src/Components/messenger/Messenger.js
+++ src/Components/messenger/Messenger.js
@@ -1,8 +1,18 @@
 import React from "react";
+import ConversationList from "./messengerComponent/conversationList";
 
 function Messenger() {
-    return(
-        <div>'hello'</div>
+    return (
+        <div className={'Container'}>
+            <div className={'rightSide'}>
+                <ConversationList/>
+            </div>
+            <div className={'leftSide'}>
+                <div className={'header'}></div>
+                <div className={'content'}></div>
+                <div className={'footer'}></div>
+            </div>
+        </div>
     )
 }
 
---
+++ src/Components/messenger/messengerComponent/conversationList.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const conversations = [
+    {
+        id: 1,
+        name: 'ali',
+        latest_message: 'hello',
+        unseen: 5,
+        image: 'https://cdn4.iconfinder.com/data/icons/linecon/512/photo-512.png',
+        email: 'ali123@gmail.com',
+    },
+    {
+        id: 1,
+        name: 'reza',
+        latest_message: 'goodby',
+        unseen: 5,
+        image: 'https://f0.pngfuel.com/png/81/570/profile-logo-png-clip-art.png',
+        email: 'reza123@gmail.com',
+    }
+]
+
+function ConversationList() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ConversationList
