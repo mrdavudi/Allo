3ce2f2b7c899904d2c4b237b6261d3dd3bae67f2
unknown <mohamadrezadavoudi.11@gmail.com>
2019-12-25 13:05:43 +0330

    add footer.css

--- src/Components/messenger/messengerComponent/footer.js
+++ src/Components/messenger/messengerComponent/footer.js
@@ -1,9 +1,19 @@
 import React from "react";
 import CustomiseInput from "../../Inputs/CustomiseInput";
 
+function getDataFromCustomiseInput(value, InputName) {
+
+}
+
 function Footer() {
-    return(
-        <CustomiseInput/>
+    return (
+        <CustomiseInput
+            name={'sendMessage'}
+            type={'text'}
+            placeHolder={'Type a message'}
+            icon={'plus-circle'}
+            onChange={(value, InputName) => getDataFromCustomiseInput(value, InputName)} //get data and save it on state
+        />
     )
 }
 
